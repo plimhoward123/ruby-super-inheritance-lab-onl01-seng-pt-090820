feebe64df885c1f2cd567b5f10436c654c81ba58
Plimon Howard <plimon.howard@gmail.com>
2020-10-15 21:00:25 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,6 +6,7 @@ class ChattyStudent < Student
   end
 
   def raise_hand
-
+    super
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
   end
 end
